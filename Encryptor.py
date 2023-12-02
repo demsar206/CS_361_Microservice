f4943ccd594db46af59ba929a9d721718198458b
demsar206 <114278599+demsar206@users.noreply.github.com>
2023-12-02 20:50:54 +0300

    Update Encryptor.py

--- Encryptor.py
+++ Encryptor.py
@@ -5,71 +5,73 @@ from cryptography.fernet import Fernet
 import json
 
 
-# Create a socket for my partner's requests
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 12345)
+def handle_client(partner_socket):
+    while True:
+        try:
+            message = partner_socket.recv(1024).decode('utf-8')
+            if not message:
+                break
 
-server.bind(server_address)
-server.listen(5)
+            message_py = json.loads(message)
+            e_message = fernet.encrypt(json.dumps(message_py).encode())
+            partner_socket.send(e_message)
+
+        except Exception as e:
+            print("Error:", e)
+            break
+
+    partner_socket.close()
 
 
-def listen_for_messages(partner_socket):
-    """Listens for an incoming message, encrypts it, and sends it back out"""
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 12345)
+    server.bind(server_address)
+    server.listen(5)
 
     while True:
         partner_socket, address = server.accept()
-        message = partner_socket.recv(1024).decode('utf-8')
-        message_py = json.loads(message)
-        e_message = fernet.encrypt(message_py)
-        e_message_json = json.dumps(e_message)
-        partner_socket.send(e_message_json.encode())
+        print("Connected to:", address)
 
-# Start the message listening thread
-message_thread = threading.Thread(target=listen_for_messages, args=(server,), daemon=True)
-message_thread.start()
+        handle_client_thread = threading.Thread(target=handle_client, args=(partner_socket,))
+        handle_client_thread.start()
 
-# Generate an encryption key
-key = Fernet.generate_key()
-fernet = Fernet(key)
-
-
+# clears text in GUI
 def clear():
-    """Clears text from the text box"""
     my_text.delete(1.0, END)
 
+# Encrypts text
 def encrypt_text():
-    """Encrypts a text"""
-
     text_to_encrypt = my_text.get(1.0, END)
     encrypted_text = fernet.encrypt(text_to_encrypt.encode())
     clear()
     my_text.insert(END, encrypted_text)
 
+# Decrypts text
 def decrypt_text():
-    """Decrypts a text"""
-    # Retrieve the text from the Text widget
     text_to_decrypt = my_text.get(1.0, END)
 
     try:
-
         decrypted_text = fernet.decrypt(text_to_decrypt.encode()).decode()
         clear()
         my_text.insert(END, decrypted_text)
-
     except Exception as e:
         print("Decryption error:", e)
 
+# Gets text from partner's microservice
 def get_text():
-    """Gets a text from my partner's microservice"""
-
     try:
         user_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        user_socket.connect(('localhost', 12345))
+        user_socket.connect(('localhost', 54654))
 
-        request = "GET_TEXT"
+        request = {
+            'request-type': 'current-conditions',
+            'city': 'Cleveland',
+            'state': 'Ohio'
+        }
+
         json_request = json.dumps(request)
         user_socket.send(json_request.encode())
-
 
         response = user_socket.recv(1024).decode()
         response_python = json.loads(response)
@@ -81,31 +83,30 @@ def get_text():
     except Exception as e:
         print("Error:", e)
 
+# User choice for warning box
 def choice(option):
-    """Clears the text box if popup option is Yes"""
-
     popup.destroy()
     if option == "Yes":
         clear()
 
+# Displays warning for clear button
 def warning():
-    """Create a warning pop-up for the clear button"""
-
     global popup
     popup = Toplevel(root)
     popup.title("warning")
     popup.geometry("250x150")
     popup.config(bg="red")
 
-    warning_label = Label(popup, text= "You are about to clear the screen. Proceed?", width=50)
+    warning_label = Label(popup, text="You are about to clear the screen. Proceed?", width=50)
     warning_label.pack(pady=10)
     warning_frame = Frame(popup)
     warning_frame.pack(pady=5)
-    yes = Button(warning_frame, text="Yes", command = lambda: choice("Yes"), bg="lightgrey")
-    yes.grid(row= 0, column=1)
-    yes = Button(warning_frame, text="No", command = lambda: choice("No"), bg = "lightgrey")
-    yes.grid(row=0, column=2)
+    yes = Button(warning_frame, text="Yes", command=lambda: choice("Yes"), bg="lightgrey")
+    yes.grid(row=0, column=1)
+    no = Button(warning_frame, text="No", command=lambda: choice("No"), bg="lightgrey")
+    no.grid(row=0, column=2)
 
+# 'About' window
 def about():
     global popup
     popup = Toplevel(root)
@@ -115,19 +116,18 @@ def about():
     about_label = Label(popup, text="Click OK to close this window:")
     about_label.pack(pady=10)
 
-    about_text = Text(popup, height=10, width=50,  wrap="word")
+    about_text = Text(popup, height=10, width=50, wrap="word")
     about_text.insert('1.0',
-                      "                    Information \n\n I’m making this program to learn about software development. "
+                      "Information \n\n I’m making this program to learn about software development. "
                       "I’m hoping to take this program and build it out into a full messaging app at some point in the  "
                       "future. Thank you for using my program. If you have any questions or comments, you can reach me "
                       "at demsarw@oregonstate.edu.\n\n"
-                      "                 Technical Details \n\n We use the Fernet class from the cryptography.fernet "
+                      "Technical Details \n\n We use the Fernet class from the cryptography.fernet "
                       "module in Python. First, we generate a random encryption key with fernet.generate_key(). Next, we "
                       "initialize the key to encrypt or decrypt the message. The Get Text key sends a request to a "
                       "microservice using Python socket and posts the text to the text window. A separate port "
                       "listens for requests from another microservice to encrypt and send back data. Threading "
                       "is used to run these two functions at the same time.")
-
     about_text.config(state='disabled')
     about_text.pack(pady=5)
 
@@ -144,12 +144,21 @@ def about():
     button_frame.pack()
 
 
-#tkinter GUI
+# Initialize encryption key
+key = Fernet.generate_key()
+fernet = Fernet(key)
+
+# Start server in a separate thread
+server_thread = threading.Thread(target=start_server)
+server_thread.start()
+
+# Run GUI in the main thread
 root = Tk()
 root.geometry("800x800")
 
-instructions = Message(root, text = "Enter a message to encrypt or click 'Get Text' and you will be provided with a text." 
-                                    "Click 'Encrypt' to encrypt your message and 'Decrypt' to decrypt your message.", width = 500)
+instructions = Message(root, text="Enter a message to encrypt or click 'Get Text' and you will be provided with a text."
+                                  "Click 'Encrypt' to encrypt your message and 'Decrypt' to decrypt your message.",
+                       width=500)
 instructions.pack()
 
 about_button = Button(root, text="About", command=about)
@@ -171,13 +180,4 @@ encrypt_button.grid(row=0, column=1)
 decrypt_button.grid(row=0, column=2)
 get_text_button.grid(row=0, column=3)
 
-
 root.mainloop()
-
-
-
-
-
-
-
-
